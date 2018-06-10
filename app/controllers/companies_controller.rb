b4f74c596ba98b3a50d10736c2ac62c56fd93ca0
Minhal <mag166@case.edu>
2018-06-10 17:39:36 -0400

    many changes includig footer and other stuff

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,18 +1,31 @@
 class CompaniesController < ApplicationController
-  def index
-    @company = Company.new
-  end
 
   def create
-    @company = Company.new
-    puts params[:symbol]
+    symbol = params[:symbol].upcase
+    @company = Company.where(symbol: symbol).first
+    if @company == nil
+      begin
+        symbolCheck = IEX::Resources::Company.get(symbol)
+      rescue IEX::Errors::SymbolNotFoundError
+        flash[:error] = "#{symbol} is not a valid Company"
+        redirect_back fallback_location: root_path
+
+      else
+        @company = Company.new(symbol:symbol)
+        if @company.save
+          flash[:success] = "Added #{symbol} to Companies"
+          redirect_to @company
+        else
+          flash[:error] = "Error adding Company"
+        end
+      end
+    else
+      redirect_to @company
+    end
   end
 
-  def new
-    @company = Company.new
+  def show
+    @company = Company.find(params[:id])
   end
 
-  def edit
-    @company = Company.new
-  end
 end
