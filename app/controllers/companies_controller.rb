89b3dbc4e7a941376d4e9267afe4e42d018d453b
Minhal <mag166@case.edu>
2018-06-10 18:51:08 -0400

    added company info

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -26,6 +26,39 @@ class CompaniesController < ApplicationController
 
   def show
     @company = Company.find(params[:id])
+    @companyInfo = IEX::Resources::Company.get(@company.symbol)
+    @shares = num_shares(@company)
+    @avg = avg_price(@company)
+    @price = IEX::Resources::Price.get(@company.symbol)
+
+  end
+
+  def num_shares(company)
+    transactions = Transaction.where(user_id:current_user.id,company_id:company.id)
+    counter = 0
+    transactions.each do |transaction|
+      if transaction.buy
+        counter = counter + transaction.shares
+      else
+        counter = counter - transaction.shares
+      end
+    end
+    return counter
+  end
+
+  def avg_price(company)
+    transactions = Transaction.where(user_id:current_user.id,company_id:company.id, buy:true)
+    numerator = 0
+    denominator = 0
+    transactions.each do |transaction|
+      numerator = numerator + (transaction.shares*transaction.price)
+      denominator = denominator + transaction.shares
+    end
+    if denominator != 0
+      return (numerator/denominator).round(3)
+    else
+      return '-'
+    end
   end
 
 end
